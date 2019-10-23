85c66c4dc551459c58ffe36ad594ff3632df24ef
tdnicola <tdnicola@gmail.com>
2019-10-23 14:20:54 -0400

    added start page layout. added name and color to be passed to chat page.

--- components/Chat.js
+++ components/Chat.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 //import relevant components from react native
-import { StyleSheet, Text, View, TextInput, Button,} from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ImageBackground} from 'react-native';
 
 // create Screen1 (Start) class
 export default class Chat extends Component {
@@ -8,15 +8,27 @@ export default class Chat extends Component {
         super(props);
     }
 
+    //pulling in information from Start.js name/color
     static navigationOptions = ({ navigation }) => {
         return {
-            title: navigation.state.params.name
+            name: navigation.state.params.name,
+            color: navigation.state.params.color,
         };
     };
 
     render(){
         return (
+            <View style={{flex: 1, backgroundColor: this.props.navigation.state.params.color}}>
                 <Text> Hello {this.props.navigation.state.params.name}</Text>
+            </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    backgroundStateColor: {
+       flex: 1,
+       //Erroring about navigation
+    //    backgroundColor: this.props.navigation.state.params.color,
+    }
+})
