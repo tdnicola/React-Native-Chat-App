45dbc051173b34c32a0b695922fd86fd6b25a7e7
tdnicola <tdnicola@gmail.com>
2019-10-26 19:17:41 -0400

    updated color for text bubbles. working to see if font color is possible as well.

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer} from 'react-navigation';
+import KeyboardSpacer from 'react-native-keyboard-spacer';
 
 import Start from './components/Start';
 import Chat from './components/Chat';
--- components/Chat.js
+++ components/Chat.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 //import relevant components from react native
 import { StyleSheet, Text, View } from 'react-native';
 
-import { GiftChat, GiftedChat } from 'react-native-gifted-chat';
+import { GiftChat, GiftedChat, Bubble } from 'react-native-gifted-chat';
+import { Platform } from '@unimodules/core';
+//only for android chat 
+import KeyboardSpacer from 'react-native-keyboard-spacer';
+
 
 export default class Chat extends Component {
     constructor(props) {
@@ -20,15 +24,37 @@ export default class Chat extends Component {
                 {
                     _id: 1,
                     text: 'Hello Developer',
+                    createdAt: new Date(),
                     user: {
                         _id: 2,
                         name: 'React Native',
                         avatar: 'https://placeimg.com/140/140/any',
                     },
                 },
+                {
+                    _id: 2,
+                    text: `Chatting with ${this.props.navigation.state.params.name}`,
+                    createdAt: new Date(),
+                    system: true,
+                }
             ],
         }) 
     }
+
+// changing the color of the text bubble
+    renderBubble(props) {
+        return (
+            <Bubble 
+                {...props}
+                wrapperStyle={{
+                    right: {
+                        backgroundColor: '#000'
+                    }
+                }}
+            />
+        )
+    }
+
 
     onSend(messages = []) {
         this.setState(previousState => ({
@@ -52,12 +78,15 @@ export default class Chat extends Component {
             >
                 <Text> Hello {this.props.navigation.state.params.name}</Text>
                 <GiftedChat
+                    renderBubble={this.renderBubble.bind(this)}
                     messages={this.state.messages}
                     onSend={messages => this.onSend(messages)}
                     user={{
                         _id: 1,
                     }}
                 />
+                {/* Keyboard spacer for android only. */}
+                {Platform.OS === 'android' ? <KeyboardSpacer /> : null}
             </View>
         )
     }
